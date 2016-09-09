9d637cb35944ea020bbbcee47ce121c51b8f9940
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-09 15:10:58 -0400

    Fixed markers object and all associated methods

--- html/js/fmc.js
+++ html/js/fmc.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+
 	var latitude = -6.565067,
 		longitude = 106.805026,
 		map_zoom = 12;
@@ -185,28 +186,7 @@ jQuery(document).ready(function($){
 			position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
     }
-	var map = new google.maps.Map(document.getElementById('map'), map_options);			
-
-	/*  
-		var marker = new google.maps.Marker({
-	  	position: new google.maps.LatLng(latitude, longitude),
-	    map: map,
-	    visible: true,
-	 	icon: marker_url,
-	}); 
-	*/
-	
-	// Try HTML5 geolocation.  If browser doesn't support, won't display anything
-	if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map.setCenter(pos);    
-            }, function() { window.alert('trigger success'); }
-        );
-	}
+	var map = new google.maps.Map(document.getElementById('map'), map_options);	
 
 	// create object prototype for markers, including markers array and relevant methods
 	var Marker_proto = {
@@ -216,13 +196,13 @@ jQuery(document).ready(function($){
 		// methods
 		create: function () {
 			var self = Object.create(this);
-			//self.params = data;
+			//self.map = map;
 			return self;
 		},
 
 		addMarker: function(place) {
 			//create new latLng object
-			var myLatLng = new google.maps.LatLng(place.latitude,place.longitude);
+			var myLatLng = new google.maps.LatLng(place.lat,place.lon);
 
 			// create new info window object
 			//var infowindow = new google.maps.InfoWindow();
@@ -234,8 +214,18 @@ jQuery(document).ready(function($){
 				raiseOnDrag: false,
 				map: map,
 				//labelContent: place.place_name
-			});
-			
+			});	
+		},
+
+		removeMarkers: function() {
+			var that = this;
+			if (this.markers) {
+				for (prop in this.markers) {
+					if (prop !== "user") {
+						this.markers[prop].setMap(null);
+					}
+				}
+			}
 		},
 
 		update: function() {
@@ -254,8 +244,8 @@ jQuery(document).ready(function($){
 			$.getJSON("update.php", parameters)
 			.done(function(data, textStatus, jqXHR) {
 				// remove old markers from map
-				//removeMarkers();
-
+				that.removeMarkers();
+				
 				// add new markers to map
 				for (var i = 0; i < data.length; i++)
 				{
@@ -265,22 +255,31 @@ jQuery(document).ready(function($){
 			.fail(function(jqXHR, textStatus, errorThrown) {
 				// log error to browser's console
 				console.log(errorThrown.toString());
-				console.log("fail");
 			});
 		}
-
+	// End prototype declaration
 	};
 
-	//pop up for registration
+	// Create new child of prototype
 	var MarkerStack = Marker_proto.create();
 	
-	//google.maps.event.addListenerOnce(map, 'bounds_changed', MarkerStack.update());
-	google.maps.event.addListenerOnce(map, 'bounds_changed', function() {
-		MarkerStack.update();
-  	});
+	// Try HTML5 geolocation.  If browser doesn't support, won't display anything
+	if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos);    
+            }, function() { window.alert('trigger success'); }
+        );
+	}
 
+	// Load markers on map (at beginning and every time map is moved)
+	google.maps.event.addListener(map, 'idle', function () { MarkerStack.update(); })
 
 });
+
 
 /* Registration/login Popup */
 $("#navRegister").click(function() {
