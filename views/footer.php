922d86d52dd83ebeea590f0b6d0ea0f44331505b
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-10 17:47:33 -0400

    Added search bar, code stable and ready for master

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,11 @@
+<?php
 
- 
-        <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyD9P8DYB0zXsO4NfnnqrX8zeofdyK5OegA'></script>
-
-        <script src="js/fmc.js"></script>
-    
+if($_GET["map"] != "pkr")
+{
+  print("<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyD9P8DYB0zXsO4NfnnqrX8zeofdyK5OegA&libraries=places'></script><script src='js/fmc.js'></script>");
+} else {
+  print("<!-- default google map and local js omitted -->");
+}
+?>
   </body>
 </html>
