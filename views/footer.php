7ca53ac851d57b88bd8d3c8442f9eed3245253b6
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-10 20:16:30 -0400

    undoing my shitty merge from branch eventform

---
+++ views/footer.php
@@ -0,0 +1,10 @@
+<?php
+if($_GET["map"] != "pkr")
+{
+  print("<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyD9P8DYB0zXsO4NfnnqrX8zeofdyK5OegA&libraries=places'></script><script src='js/fmc.js'></script>");
+} else {
+  print("<!-- default google map and local js omitted -->");
+}
+?>
+  </body>
+</html>
