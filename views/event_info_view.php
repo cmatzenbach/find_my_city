d203c3bf74a0566acc91181b8be28fde36b87ad6
zNeill <zneill@gmail.com>
2016-09-12 19:43:32 -0400

    finished RSVP

--- views/event_info_view.php
+++ views/event_info_view.php
@@ -7,18 +7,35 @@
     <?php 
     if(isset($_SESSION["user_id"]))
     {
+        //if event owner print event button
         if($owner["id"] == $_SESSION["user_id"]) 
         {
             print('<span style="float:right"><button type="submit" id="editEventButton" class="btn btn-primary">Edit Event</button></span>');
         }
 
-        if($data["status"] != "full")
+       // print("IS ATTENDING: ");
+        //echo $isAttending;
+        //print("<BR>");
+        
+        //if user is NOT attending the event
+        if(!($isAttending == "yes"))
         {
-            print('<a href="https://findmy.city/rsvp.php?mode=Y&e_id=' . $data["id"] . '"><button type="submit" class="btn btn-primary">RSVP YES</button></a> <button type="submit" id="rsvpNo" class="btn btn-primary" onClick="RSVP(N)">RSVP NO</button>');
+            //print("SESSION USER ID: " . $_SESSION["user_id"]);
+            //if event is full
+            if($data["status"] == "full" )
+            {
+                print('<h4>Sorry, this event cannot accept additional attendees!</h4>');
+            }else{
+            //if event is not full
+                print('<a href="https://findmy.city/rsvp.php?mode=Y&e_id=' . $data["id"] . '"><button type="submit" class="btn btn-primary">RSVP YES</button></a>');
+            }
         }else{
-            print('<h4>Sorry, this event cannot accept additional attendees!</h4>');
+        //else user IS attending the event
+            print('<a href="https://findmy.city/rsvp.php?mode=N&e_id=' . $data["id"] . '"><button type="submit" class="btn btn-primary">RSVP NO</button></a>');
         }
     }else{
+        //user needs to login
+        //see nav class = navRegister in nav.js if you're confused'
         print('<p><a href="javascript:;" class="navRegister">Login</a> to confirm attendance at the event. The event organizer may not accept unregistered guests.</p>');
     } ?>
 
@@ -65,8 +82,14 @@ $(".navRegister").click(function() {
         <?php 
         if($attendeeCount <= 50)
         {
-            foreach($attendees as $attendee){
-                print("<li>". $attendee . "</li>");
+            foreach($attendees as $attendee)
+            {
+                foreach($attendee as $key => $value)
+                {
+                    if($key == "displayName"){
+                        print("<li>" . $value . "</li>");
+                    }
+                }
             }
         }else{
             print("</ul><p>There are over 50 attendees!You probably don't need to know their usernames, that would look terrible on the page!</p>");
