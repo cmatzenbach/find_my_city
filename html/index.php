8bbb632e3fc3e3ea8a819ba3495d7b61e1a158d0
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-12 18:09:07 -0400

    cleaned up some console logs

--- html/index.php
+++ html/index.php
@@ -1,10 +1,10 @@
 <?php
         
-        print_r(json_encode($_POST, JSON_HEX_TAG));
         echo '<script>var filters = ';
         echo json_encode($_POST, JSON_HEX_TAG);
-        echo ';console.log(filters);</script>';
+        echo ';</script>';
 
         include("../private/helpers.php");
         render("map.php");
+        
 ?>
