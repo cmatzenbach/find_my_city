29a43c9cf69fe16807d20c9bfcf50f675a54bd52
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-13 02:27:01 -0400

    BUILT THE GODDAMN FILTER

--- html/index.php
+++ html/index.php
@@ -1,10 +1,20 @@
 <?php
-        
-        echo '<script>var filters = ';
-        echo json_encode($_POST, JSON_HEX_TAG);
-        echo ';</script>';
+    session_start();
 
-        include("../private/helpers.php");
-        render("map.php");
-        
+	// mode is set, see if it is for updateFilters
+	if (isset($_POST["mode"])) {
+		if ($_POST["mode"] === "updateFilters") {
+			foreach ($_POST as $key => $value) {
+				$_SESSION[$key] = $value;
+			}
+		}
+
+		echo '<script>var filters = ';
+		echo json_encode($_POST, JSON_HEX_TAG);
+		echo ';</script>';
+	}
+
+	include("../private/helpers.php");
+	render("map.php");
+	
 ?>
--- views/filter_events_form.php
+++ views/filter_events_form.php
@@ -3,30 +3,30 @@
 <form action="index.php" method="POST">
 
     <div class="form-group">
-        <label for="carrier">Filter By Event Type</label>
+        <label for="category">Filter By Event Category</label>
         <select class="form-control" id="category" name="category" aria-describedby="categoryHelp">
-            <option value=''>-- Select One --</option>
+            <option value=''>-- Display All --</option>
             <option value='basketball'>Basketball</option>
             <option value='beach'>Beach</option>
             <option value='baseball'>Baseball</option>
-            <option value='bicycling'>Bicycling</option>
             <option value='basketball'>Bowling</option>
             <option value='cultural'>Cultural</option>
+            <option value='cycling'>Cycling</option>
             <option value='football'>Football</option>
-            <option value='football'>Frisbee</option>
+            <option value='frisbee'>Frisbee</option>
             <option value='jobs'>Job Fair</option>
             <option value='musicplay'>Music (playing instruments)</option>
             <option value='musiclisten'>Music (appreciation/concerts)</option>
             <option value='party'>Party</option>
             <option value='picnic'>Picnic</option>
             <option value='political'>Political</option>
-            <option value='random'>Random</opt0ion>
+            <option value='random'>Random</option>
             <option value='religious'>Religious</option>  
             <option value='study'>Study Group</option>
             <option value='tabletop'>Tabletop Games</option>
             <option value='technology'>Technology</option>
             <option value='tennis'>Tennis</option>
-            <option value='tennis'>Volleyball</option>
+            <option value='volleyball'>Volleyball</option>
             <option value='weightlifting'>Weight Lifting</option>
             <option value='yoga'>Yoga</option>
             <option value='other'>Other</option>
@@ -35,21 +35,63 @@
       </div>
 
       <div class="form-group">
-        <label for="carrier">Filter By Event Status</label>
+        <label for="status">Filter By Event Status</label>
         <select class="form-control" id="status" name="status" aria-describedby="categoryHelp">
-            <option value=''>-- Select One --</option>
+            <option value=''>-- Display All --</option>
             <option value='pending'>Pending</option>
-            <option value='pending'>Active</option>
-            <option value='pending'>Full</option>
+            <option value='active'>Active</option>
+            <option value='full'>Full</option>
         </select>
         <small id="categoryHelp" class="form-text text-muted">Please select a category to filter by.</small>
      </div>
 
+     <input type="hidden" name="mode" id="mode" value="updateFilters" />
+
      <div class="form-group">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
-    
+    <div id="filter-load"></div>
 </form>
 
 </div>
 <style>.modal-footer { display:none }</style>
+<script>
+    function prepopulateFilters() {
+      if ($("#filter-load").is(":visible")) {
+ 
+          // Get variables that will pre-populate filter selections
+          <?php 
+          print("var filterCategory = \"" . $_SESSION["category"] . "\";");
+          print("var filterStatus = \"" . $_SESSION["status"] . "\";");
+          ?>
+
+          if (filterCategory) {
+            var categoryNode = document.getElementById('category');
+            var opts = categoryNode.options;
+            for (var opt, j = 0; opt = opts[j]; j++) {
+                if (opt.value == filterCategory) {
+                    categoryNode.selectedIndex = j;
+                    break;
+                }
+            }
+          }
+          if (filterStatus) {
+            var statusNode = document.getElementById('status');
+            var opts2 = statusNode.options;
+            for (var opt2, h = 0; opt2 = opts2[h]; h++) {
+                if (opt2.value == filterStatus) {
+                    statusNode.selectedIndex = h;
+                    break;
+                }
+            }
+          }
+      }
+    else {
+        setTimeout(prepopulateFilters, 50); //wait 50 ms, then try again
+    }
+    }
+
+    jQuery(document).ready(function() {
+        prepopulateFilters();
+    });
+</script>
