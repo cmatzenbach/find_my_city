912f13be25d319c76cd591b079c2bc87c45a61a5
Chris Matzenbach <cmatzenbach@gmail.com>
2016-09-13 22:32:58 -0400

    added delete event controller

---
+++ html/delete_event.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once("../private/helpers.php");
+require_once("../private/sql.php");
+
+// Must have Session User ID, GET Event ID and GET Mode or else EXIT
+if(!isset($_SESSION["user_id"])) {
+    render("message.php", ["message" => "You must login to use this page!"]);
+}
+if(!isset($_GET["e_id"])) {
+    render("message.php", ["message" => "Something's not right. Email tech the webmaster with this phrase: Event id missing from rsvp request."]);
+}
+
+// Pull Information about the event from the database
+$eventQuery = $pdo->prepare("SELECT * FROM event WHERE id = ?");
+$eventQuery->execute(array($_GET["e_id"]));
+$eventInfo = $eventQuery->fetchAll(PDO::FETCH_ASSOC);
+
+// This means they are not the owner of event trying to be deleted
+if ($_SESSION["user_id"] != $eventInfo["user_id"]) {
+    render("message.php", ["message" => "You are not allowed to delete other users' events!"]);
+}
+
+// If we're at this point, ok to delete event from table
+$deleteEvent = $pdo->prepare("DELETE FROM event WHERE id = ?");
+$deleteEvent->execute($_GET["e_id"]);
+$evtDel = $deleteEvent->rowCount();
+print_r($evtDel . " event(s) deleted\n");
+if ($evtDel === 0) render("message.php", ["message.php","Error: Could not delete event. Please contact the devs."]);
+
+// Now need to delete all attendance records for event
+$deleteAtt = $pdo->prepare("DELETE * FROM attendance WHERE event_id = ?");
+$deleteAtt->execute($_GET["e_id"]);
+$attDel = $deleteAtt->rowCount();
+print_r($attDel . " attendances deleted\n");
+if ($attDel === 0) render("message.php", ["message" => "Error: Could not remove attendance records for event.  Please contact the devs."]);
+
+render("message.php", "Event successfully deleted!");
+
+
+?>
